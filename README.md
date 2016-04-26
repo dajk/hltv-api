56f45b122829f1d10261e77afa3312bf577df2f0
Radovan Hajdukovic <hajdukovic.radovan@gmail.com>
2016-04-26 03:58:24 +0200

    add news 📰 , merge methods in one prototype getData function

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# hltv-api
+## hltv-api
+
+### Installation
+`npm install hltv-api --save`
+
+### Methods
+
+#### Get upcoming matches
+```
+var upcomingMatches = require('hltv-api').getUpcomingMatches;
+
+upcomingMatches.getData(function(games) {
+  for(var i; i < games.length; i++) {
+    console.log(games[i]);
+  }
+})
+```
+
+#### Get latest news
+```
+var latestNews = require('hltv-api').getLatestNews;
+
+latestNews.getData(function(news) {
+  for(var i; i < news; i++) {
+    console.log(news[i]);
+  }
+})
+```
--- src/index.js
+++ src/index.js
@@ -1,37 +1,42 @@
 var request = require('request');
 var parseString = require('xml2js').parseString;
 
-var upcomingGames = {
-  url: 'http://www.hltv.org/hltv.rss.php'
+function HLTV(type) {
+  this.type = type;
+}
+
+HLTV.prototype.getUrl = function() {
+  return 'http://www.hltv.org/' + this.type + '.rss.php';
 };
 
-upcomingGames.getUpcoming = function(callback) {
-  var gamesAttr = {};
-  request({ uri: upcomingGames.url }, function(error, response, body) {
+HLTV.prototype.getData = function(callback) {
+  var attr = {};
+  request({ uri: this.getUrl() }, function(error, response, body) {
     parseString(body, function(err, result) {
       if (err) throw err;
       
-      gamesAttr = {
-        'upcomingLength': result.rss.channel[0].item.length,
-        'games': {}
+      attr = {
+        'callbackLength': result.rss.channel[0].item.length,
+        'object': {}
       };
       
-      for (var i = 0; i < gamesAttr.upcomingLength; i++) {
-        var matchObj = {
-          team1 : result.rss.channel[0].item[i].title[0].split(' vs ')[0],
-          team2 : result.rss.channel[0].item[i].title[0].split(' vs ')[1],
+      for (var i = 0; i < attr.callbackLength; i++) {
+        var obj = {
           title : result.rss.channel[0].item[i].title[0],
           link  : result.rss.channel[0].item[i].link[0],
-          event : result.rss.channel[0].item[i].description[0],
+          description : result.rss.channel[0].item[i].description ? result.rss.channel[0].item[i].description[0] : null,
           date  : result.rss.channel[0].item[i].pubDate[0]
         };
         
-        gamesAttr.games[matchObj.link.match(/\/match\/([0-9]+)/, '')[1]] = matchObj;
+        attr.object[i] = obj;
       }
       
-      callback(gamesAttr);
+      callback(attr);
     });
   });
-};
+}
 
-module.exports = upcomingGames;
+module.exports = {
+  getUpcomingMatches: new HLTV('hltv'),
+  getLatestNews: new HLTV('news')
+};
