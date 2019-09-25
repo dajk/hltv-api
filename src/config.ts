8f33136bdd0b9c717f468c8c553d9ad8b7a06a17
Samuel Papke <samuel.papke@protonmail.com>
2019-09-25 09:40:38 +0200

    =Extracting Matches constructor into a function.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,28 @@
+interface maps {
+  [key: string]: string
+  trn: string
+  mrg: string
+  d2: string
+  inf: string
+  vtg: string
+  ovp: string
+  nuke: string
+}
+
 export const CONFIG = {
   BASE: 'https://www.hltv.org',
   STATIC: 'https://static.hltv.org',
   RSS: '/rss',
   RESULTS: '/results',
+  MATCHES: '/matches',
+}
+
+export const MAPS: maps = {
+  trn: 'Train',
+  mrg: 'Mirage',
+  d2: 'Dust 2',
+  inf: 'Inferno',
+  vtg: 'Vertigo',
+  ovp: 'Overpass',
+  nuke: 'Nuke',
 }
--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,6 @@ import Matches from './matches'
 export const getNews = (cb: (response: any) => void) => new RSS('news', cb)
 export const getResults = (cb: (response: any) => void) => new Results(cb)
 export const getMatches = (id: string, cb: (response: any) => void) =>
-  new Matches(id, cb)
+  new Matches(id, cb).getSingleMatch()
+export const getAllMatches = (cb: (response: any) => void) =>
+  new Matches('false', cb).getAllMatches()
