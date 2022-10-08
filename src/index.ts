89384d53ce5f03a7ead3129f9bb9d8e2f3468557
Captain Zidgel <13912938+CaptainZidgel@users.noreply.github.com>
2022-10-08 13:52:45 -0500

    Added getPlayersByName

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { getMatchById } from './match'
 import { getMatches } from './matches'
 import { getResults } from './results'
 import { getTopPlayers } from './players'
-import { getPlayerById } from './player'
+import { getPlayerById, getPlayersByName } from './player'
 import { getTopTeams } from './teams'
 import { getTeamById } from './team'
 import getRSS from './rss'
@@ -14,6 +14,7 @@ export default {
   getMatches,
   getTopPlayers,
   getPlayerById,
+  getPlayersByName,
   getTopTeams,
   getTeamById,
 }
